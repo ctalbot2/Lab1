b44ba08deaf208124d8606afb12f23efbfb955ef
Cole Talbot <ctalbot2@student.cscc.edu>
2026-09-20 09:37:45 -0400

    Filled out the rest of the options

--- Lab1_ctalbot2-2.py
+++ Lab1_ctalbot2-2.py
@@ -16,4 +16,15 @@ while True:
     if choice == "1":
         radius = int(input("Enter the radius of your circle "))
         print(f"The area of the circle is {circle_area(radius)}")
+    elif choice == "2":
+        radius = int(input("Enter the radius of your circle "))
+        print(f"The circumference of your circle is {circle.calc_circumference(radius)}")
+    elif choice == "3":
+        width = int(input("Enter the width of your rectangle "))
+        height = int(input("Enter the height of your rectangle "))
+        print(f"The area of your rectangle is {rectangle_area(width, height)}")
+    elif choice == "4":
+        width = int(input("Enter the width of your rectangle "))
+        height = int(input("Enter the height of your rectangle "))
+        print(f"The perimeter of your rectangle is {rectangle.calc_perimeter(width, height)}")
     break
